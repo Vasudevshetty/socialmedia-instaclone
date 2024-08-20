5934d8dbdebf45c1282803ba49c533047d0c6e40
Vasudevshetty <vasudeepu2815@gmail.com>
2024-08-21 00:18:10 +0530

    Post controller work done

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const sharp = require("sharp");
 const Post = require("../model/post.model");
+const User = require("../model/user.model");
+const Comment = require("../model/comment.model");
 const cloudinary = require("../utils/cloudinary");
 
 async function addNewPost(req, res) {
@@ -106,8 +108,187 @@ async function getUserPosts(req, res) {
   }
 }
 
+async function likePost(req, res) {
+  try {
+    const likedBy = req.id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post)
+      return res.status(404).json({
+        message: "Post not found",
+        success: false,
+      });
+
+    await post.updateOne({ $addToSet: { likes: likedBy } });
+    await post.save();
+
+    // implement socket io for realtime notification
+    return res.status(200).json({
+      message: "Post liked",
+      success: false,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message || err, error: true });
+  }
+}
+
+async function dislikePost(req, res) {
+  try {
+    const dislikedBy = req.id;
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    if (!post)
+      return res.status(404).json({
+        message: "Post not found",
+        success: false,
+      });
+
+    await post.updateOne({ $pull: { likes: dislikedBy } });
+    await post.save();
+
+    // implement socket io for realtime notification
+    return res.status(200).json({
+      message: "Post disliked",
+      success: false,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message || err, error: true });
+  }
+}
+
+async function addComment(req, res) {
+  try {
+    const postId = req.params.id;
+    const commentedBy = req.id;
+
+    const { text } = req.body;
+    if (!text)
+      return res
+        .status(400)
+        .json({ message: "Text not found to comment!", success: false });
+
+    const post = await Post.findById(postId);
+    if (!post)
+      return res
+        .status(404)
+        .json({ message: "Post not found", success: false });
+
+    const comment = await Comment.create({
+      text,
+      author: commentedBy,
+      post: postId,
+    }).populate({
+      path: "author",
+      select: "username, profilePic",
+    });
+
+    post.comments.push(comment._id);
+    await post.save();
+
+    return res.status(200).json({
+      message: "Commented successfully",
+      comment,
+      success: false,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message || err, error: true });
+  }
+}
+
+async function getCommentsOfPost(req, res) {
+  try {
+    const postId = req.params.id;
+    const comments = await Comment.find({
+      post: postId,
+    }).populate({ path: "author", select: "username, profilePic" });
+
+    if (!comments)
+      return res.status(404).json({
+        message: "No comments for this post",
+        success: false,
+      });
+
+    return res.status(200).json({
+      comments,
+      success: true,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message || err, error: true });
+  }
+}
+
+async function deletePost(req, res) {
+  try {
+    const postId = req.params.id;
+    const authorId = req.id;
+    const post = await Post.findById(postId);
+    if (!post)
+      return res
+        .status(404)
+        .json({ message: "Post unavaliable to delete", success: false });
+
+    if (post.author.toString() !== authorId)
+      return res.status(403).json({
+        message: "Unauthorized, only users can delete",
+      });
+
+    await post.findByIdAndDelete(postId);
+
+    let user = await User.findById(authorId);
+    user.posts = user.posts.filter((id) => id.toString() !== postId);
+    await user.save();
+
+    await Comment.deleteMany({ post: postId });
+    return res.status(200).json({ message: "Post deleted", success: true });
+  } catch (err) {
+    return res.status(500).json({ message: err.message || err, error: true });
+  }
+}
+
+async function bookmarkPost(req, res) {
+  try {
+    const postId = req.params.id;
+    const authorId = req.id;
+
+    const post = await Post.findById(postId);
+    if (!post)
+      return res
+        .status(404)
+        .json({ message: "Post not found", success: false });
+
+    const user = await User.findById(authorId);
+    if (user.bookmarks.includes(post._id)) {
+      // remove bookmark has it is already
+      await user.updateOne({ $pull: { bookmarks: post._id } });
+      await user.save();
+      return res.status(200).json({
+        type: "Unsaved",
+        message: "Unbookmarked successfully",
+        success: true,
+      });
+    } else {
+      // add to bookmark
+      await user.updateOne({ $addToSet: { bookmarks: post._id } });
+      await user.save();
+      return res.status(200).json({
+        type: "Saved",
+        message: "Bookmarked successfully",
+        success: true,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message || err, error: true });
+  }
+}
+
 module.exports = {
   addNewPost,
   getAllPosts,
   getUserPosts,
+  likePost,
+  dislikePost,
+  addComment,
+  deletePost,
+  getCommentsOfPost,
+  bookmarkPost,
 };
