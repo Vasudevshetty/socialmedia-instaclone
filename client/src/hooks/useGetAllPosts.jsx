c1ff9a16a4557b55876ac74e201c355d1a035f72
Vasudevshetty <vasudeepu2815@gmail.com>
2024-08-23 19:54:08 +0530

    Added use get all posts hooks, and working on the home page.

---
+++ client/src/hooks/useGetAllPosts.jsx
@@ -0,0 +1,30 @@
+import { setPost } from "@/redux/postSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "sonner";
+
+function useGetAllPosts() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function getAllPosts() {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_APP_BACKEND_URL}/post/all`,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(res.data);
+        if (res.data.success) {
+          dispatch(setPost(res.data.posts));
+        }
+      } catch (error) {
+        toast.error(error.response?.data?.message);
+      }
+    }
+    getAllPosts();
+  });
+}
+
+export default useGetAllPosts;
